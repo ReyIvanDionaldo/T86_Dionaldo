b0bcad3566d212cb4fac48e2a4c6d36c8f17f7c7
ReyIvanDionaldo <89230422+ReyIvanDionaldo@users.noreply.github.com>
2021-09-30 15:38:12 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,19 @@
 
 int main()
 {
-    int age = 19;
-    printf("I'm Rey Ivan Dionaldo , Age of %d years old", age);
+    int num1;
+    int num2;
+    int num3;
+    printf("Enter the first number.");
+    scanf("%d", &num1);
+    printf("Enter the second number.");
+    scanf("%d", &num2);
+    printf("Enter the third number.");
+    scanf("%d",&num3);
+
+    int result = (num1 / num2) * num3;
+    //result
+    printf("%d RPM divide %d secs multiply %d degree is %d per second",num1,num2,num3,result);
     return 0;
+
 }

952ccbee351bf7300fbabc1b47b97c8270a71841
ReyIvanDionaldo <89230422+ReyIvanDionaldo@users.noreply.github.com>
2021-11-04 14:50:10 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -1,79 +1,11 @@
-/******************************************************************************
-
-Welcome to GDB Online.
-GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
-C#, VB, Perl, Swift, Prolog, Javascript, Pascal, HTML, CSS, JS
-Code, Compile, Run and Debug online from anywhere in world.
-
-*******************************************************************************/
 #include <stdio.h>
+#include <stdlib.h>
+
 int main()
 {
-int choices, price, payment, change;
-printf("[1]Pepsi(90)\n[2]Blue water(100)\n[3]Red Bull(70)\n[4]Mountain Dew(80)\n");
-printf("Choice: ");
-scanf("%d", &choices);
-
-switch (choices){
-
-  case 1:
-  price = 90;
-  printf("insert bill: ");
-  scanf("%d", &payment);
-  if(payment >=price){
-    printf("you have purchased Pepsi, Thank you and Enjoy!.\n");
-    change = payment - price;
-    printf("Change: %d", change); 
-  } else {
-    printf(" Insufficient funds");
-  }
-  break;
-
-
-  case 2:
-  price = 100;
-  printf("insert bill: ");
-  scanf("%d", &payment);
-  if (payment >=price){
-  printf("you have purchased Blue water, Thank you and Enjoy!\n");
-  change = payment - price;
-    printf("Change: %d", change); 
-  } else {
-    printf(" Insufficient funds");
-  }
-  break;
-
-  case 3:
-  price = 70;
-  printf("insert bill: ");
-  scanf("%d", &payment);
-  if(payment >=price){
-    printf("you have purchased red bull, Thank you and Enjoy!.\n");
-    change = payment - price;
-    printf("Change: %d", change);
-  } else {
-    printf(" Insufficient funds");
-  }
-
-  break;
-
-  case 4:
-  price = 80;
-  printf("insert bill: ");
-  scanf("%d", &payment);
-  if (payment >=price){
-  printf("you have purchased Coke, Thank and Enjoy!\n");
-  change = payment - price;
-    printf("Change: %d", change); 
-  }
-   else {
-    printf(" Insufficient funds");
-  }
-  break;
-  
-
-  default:
-  printf("Invalid Input");
-}
-return 0;
+      for(int i = 1; i <= 9;i++){
+        for(int j = i; j <= 9;j++){
+      j == 9 ?  printf("%d\n",i) : printf("%d",j);
+        }
+      }
 }
